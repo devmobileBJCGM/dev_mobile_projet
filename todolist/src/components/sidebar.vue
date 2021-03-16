9141468804b3e3db2acf9399b79a5c78200b4b04
Besnard Quentin <21801101@etu.unicaen.fr>
2021-03-16 10:23:04 +0100

    première affichage de todos dans des list

---
+++ todolist/src/components/sidebar.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="sidebar">
+        <label>{{name}}</label>
+        <ul>
+            <li v-for="todo in todos" :key="todo.id">
+                <listask :id="todo.id" :completed="todo.completed" :task="todo.name"></listask>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+    import { mapGetters } from "vuex";
+    import listask from '@/components/sidebaritem.vue';
+    
+    export default {
+        name: 'sidebar',
+        components: {
+            listask
+        },
+        data() {
+            return {
+                todos: [
+                    {
+                        id: 1,
+                        name: 'tache 1',
+                        completed: true
+                    },
+                    {
+                        id: 2,
+                        name: 'tache 2',
+                        completed: false
+                    },
+                    {
+                        id: 3,
+                        name: 'tache 3',
+                        completed: false
+                    },
+                    {
+                        id: 4,
+                        name: 'tache 4',
+                        completed: true
+                    }
+                ]
+            }
+        },
+        props: {
+            id: {type: Number, default: 1},
+            completed: {type: Boolean},
+            name: {type: String}
+        },
+        computed:{
+            ...mapGetters('immo', ['getTodo']),
+            todo(){
+                return this.data()[this.id]
+            }
+        }
+    }
+</script>
