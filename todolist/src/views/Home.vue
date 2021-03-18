b82428e3d4d0c3547628b0a67ea33b219691d2a9
Besnard Quentin <21801101@etu.unicaen.fr>
2021-03-18 15:11:01 +0100

    ajout/supp de todo dans des todolist

--- todolist/src/views/Home.vue
+++ todolist/src/views/Home.vue
@@ -8,16 +8,25 @@
                     <button @click.prevent="deleteTodo(elem_list_todo)">.</button>
                 </li>
             </ul>
-            <label for="newTodoListName">tache : </label>
+            <label for="newTodoListName">Todo liste : </label>
             <input type="text" name="newTodoListName" v-model="newTodoListName">
             <button @click.prevent="createTodoList">Add New Todo liste</button>
         </div>
         <div id="List_task">
             <ul>
-                <li v-for="todo in filteredTodos" :key="todo.id">
-                    <listask :id="todo.id" :completed="todo.completed" :task="todo.name"></listask>
+                <li v-for="todo in filteredTodos" :key="todo.id" :style="colorStyle(todo.completed)">
+                    <div>
+                        <input type="checkbox" v-model="todo.completed" >
+                        <button @click.prevent="deleteTodoElement(filteredTodos,todo)">.</button>
+                        <listask :id="todo.id" :completed="todo.completed" :task="todo.name"></listask>
+                    </div>
                 </li>
             </ul>
+            <div v-if="this.idTodoSelected != -1">
+                <label for="newTodoName">Todo : </label>
+                <input type="text" name="newTodoName" v-model="newTodoName">
+                <button @click.prevent="createTodo(this.idTodoSelected)">Add New Todo</button>
+            </div>
         </div>
     </div>
 </template>
@@ -35,6 +44,7 @@ export default {
             return {
                 idTodoSelected: 1,
                 newTodoListName: '',
+                newTodoName: '',
                 list_todos: [
                     {
                         id: 1,
@@ -150,14 +160,15 @@ export default {
         methods: {
             selectTodoList(elem){
                 this.idTodoSelected = this.list_todos.indexOf(elem);
-                console.log(this.idTodoSelected); 
-                console.log(this.list_todos);
             },
             deleteTodo(elem){
-                console.log("elem " +elem);
                 let index = this.list_todos.indexOf(elem);
                 this.list_todos.splice(index, 1);
                 this.idTodoSelected = -1;
+            },
+            deleteTodoElement(list,elem){
+                let index = list.indexOf(elem);
+                list.splice(index, 1);
             },
             createTodoList(){
                 if(this.newTodoListName != ''){
@@ -167,9 +178,23 @@ export default {
                         name: this.newTodoListName,
                         completed: false,
                         todos: [] 
-                    });  
+                    });
+                    this.newTodoListName ='';
                 }
-                console.log(this.list_todos.length); 
+            },
+            createTodo(id){
+                if(this.newTodoName != ''){
+                    this.list_todos[id]["todos"].push( 
+                    {
+                        id: this.list_todos[id]["todos"].length+1,
+                        name: this.newTodoName,
+                        completed: false,
+                    });  
+                    this.newTodoName = '';
+                }
+            },
+            colorStyle(bool) {
+                return [ bool ? "color:green" : "color:red"];
             }
         },
         computed: {
@@ -184,11 +209,11 @@ export default {
 </script>
 
 <style>
-#Todo {width: 60%; margin: 0 auto;display:grid; grid-template-columns: 1fr 1fr;}
+#Todo {width: 70%; margin: 0 auto;display:grid; grid-template-columns: 1fr 1fr;}
 #List_todo ul li{padding: 5px; margin:2px; border: 1px solid black; padding: 3px; border-radius: 3px;}
 #List_todo ul li:hover {background: linear-gradient(#e8e8e8 , #f1f1f1 , #e8e8e8 );}
 #List_task ul li{text-align: left;}
-
+#List_task input{margin-right: 30px}
 #List_todo {padding-right: 20px;}
 #List_task {border-left: 3px solid black}
 
